39cbf44d78452558c4080a723adf64455573225d
João Pellacani <eng.joaopellacani@gmail.com>
2025-10-24 15:39:22 -0300

    Project Congiguration

--- esp32_smartenv/src/main.cpp
+++ esp32_smartenv/src/main.cpp
@@ -1,18 +1,24 @@
 #include <Arduino.h>
+#define LED 2
 
 // put function declarations here:
-int myFunction(int, int);
+//int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  //int result = myFunction(2, 3);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED, HIGH);
+  delay(2000);
+  digitalWrite(LED, LOW);
+  delay(500);
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+//nt myFunction(int x, int y) {
+//  return x + y;
+//}
